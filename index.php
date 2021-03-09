4a6ffbea4bfd525670f5eac39ff242f5ff3791f3
UnPotDeYaourt <66166281+UnPotDeYaourt@users.noreply.github.com>
2021-03-09 16:30:20 +0100

    Mise en place tableau de bord admin

--- index.php
+++ index.php
@@ -30,24 +30,26 @@ require('./class/UITools.php');
                 case "disconnect":
                     include('./routeur/disconnect.php');
                     break;
-                case "gestionmct":
-                    include('./view/Administration/MotCle_Theme_Gestion.php');
-                    break;
+                
                 case "settings":
                     include('./view/settings.php');
                     break;
                 case "theme":
                     include('./view/theme.php');
                     break;
-                case "mcgestion":
-                    include('./view/Administration/MotCle_Theme_Gestion.php');
-                    break;
-                case "themegestion":
-                        include('./view/settings.php');
-                        break;
+                
                 case "upload":
                         include('./view/upload.php');
                         break;
+                default:
+                    include('./view/404.html');
+                    break;
+            }
+        } else if(isset($_GET['admin']) && $user['IdProfil'] == 1){
+            switch($_GET['admin']) {
+                case "gestionmct":
+                    include('./view/Administration/MotCle_Theme_Gestion.php');
+                    break;
                 default:
                     include('./view/404.html');
                     break;
